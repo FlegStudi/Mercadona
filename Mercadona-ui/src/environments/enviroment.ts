6179f115b300b58b57fab4e7cdf20a527f45d74c
Fleg <barbora.flegelova@studi.fr>
2023-11-17 18:37:32 +0100

    README.md updated

--- Mercadona-ui/src/environments/enviroment.ts
+++ Mercadona-ui/src/environments/enviroment.ts
@@ -1,5 +1,5 @@
 
-//export const environment = {
-  //production: true,
-  //apiUrl: 'https://'
-//};
+export const environment = {
+  production: true,
+  apiUrl: 'https://flegstudi.github.io/mercadona.github.io/'
+}
